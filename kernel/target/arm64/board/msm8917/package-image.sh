5f4d60640b7dc5493be6d2e76f12741f10980dcb
Ataberk Özen <ataberkozen123@gmail.com>
2020-06-27 09:36:27 +0300

    msm8917: use msm8937 as board name in boot image
    The SOC msm8917 is based on msm8937 and uses msm8937 as board name.
    
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>

--- kernel/target/arm64/board/msm8917/package-image.sh
+++ kernel/target/arm64/board/msm8917/package-image.sh
@@ -12,6 +12,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ZIRCON_DIR=${DIR}/../../../../..
 SCRIPTS_DIR=${ZIRCON_DIR}/scripts
 
-${SCRIPTS_DIR}/package-image.sh -a -b msm8917 \
+${SCRIPTS_DIR}/package-image.sh -a -b msm8937 \
     -d $ZIRCON_DIR/kernel/target/arm64/board/msm8917/j4primelte.dtb -D append \
     -g -m -M 0x00000000 $@
